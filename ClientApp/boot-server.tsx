84986cc49ff5c0c00e71413644d5b2cb3cdabe3a
bharney <bharney0@gmail.com>
2018-06-26 08:17:30 -0700

    Fixed small issue with SSR.

--- ClientApp/boot-server.tsx
+++ ClientApp/boot-server.tsx
@@ -15,26 +15,34 @@ const stats = require('./dist/react-loadable.json');
 export default createServerRenderer(params => {
     return Loadable.preloadAll().then(() => {
         return new Promise<RenderResult>((resolve, reject) => {
-            var importFiles = JSON.parse(params.data);
+            var serverParams = JSON.parse(params.data);
             let criticalStyles = "";
             let criticalScripts = "";
             let mainStyles = "";
             let mainScripts = "";
-            if (importFiles.Styles) {
-                let files = importFiles.Styles;
-                criticalStyles = files.filter(bundle => bundle.includes('critical')).map(style => {
+            let vendorScripts = "";
+            let vendorStyles = "";
+            if (serverParams.Styles) {
+                let files = serverParams.Styles;
+                criticalStyles = files.filter(bundle => bundle.includes('critical.css')).map(style => {
                     return `<link href="${style}" rel="stylesheet"/>`
                 }).join('\n')
-                mainStyles = files.filter(bundle => bundle.includes('bundle')).map(style => {
+                vendorStyles = files.filter(bundle => bundle.includes('vendor.css')).map(style => {
+                    return `<link href="${style}" rel="stylesheet"/>`
+                }).join('\n')
+                mainStyles = files.filter(bundle => bundle.includes('bundle.css')).map(style => {
                     return `<link href="${style}" rel="stylesheet"/>`
                 }).join('\n')
             }
-            if (importFiles.Scripts) {
-                let files = importFiles.Scripts;
-                criticalScripts = files.filter(bundle => bundle.includes('critical')).map(script => {
+            if (serverParams.Scripts) {
+                let files = serverParams.Scripts;
+                criticalScripts = files.filter(bundle => bundle.includes('critical.js')).map(script => {
                     return `<script charset="utf-8" src="${script}"></script>`
                 }).join('\n')
-                mainScripts = files.map(script => {
+                vendorScripts = files.filter(bundle => bundle.includes('vendor.js')).map(script => {
+                    return `<script charset="utf-8" src="${script}"></script>`
+                }).join('\n')
+                mainScripts = files.filter(bundle => bundle.includes('bundle.js')).map(script => {
                     return `<script charset="utf-8" src="${script}"></script>`
                 }).join('\n')
             }
@@ -67,17 +75,16 @@ export default createServerRenderer(params => {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <meta name="description" content="Starter Pack for React with ASP.NET Core" />
                     <title>Starter Pack</title>
-                    <style>
-                    html,body{overflow-x:hidden}body{padding-top:70px}#custom-nav{padding-top:1em;padding-bottom:1em;opacity:1;box-shadow:none;z-index:999;box-shadow:0 4px 5px 0 rgba(0,0,0,.14),0 1px 10px 0 rgba(0,0,0,.12),0 2px 4px -1px rgba(0,0,0,.2);-webkit-font-smoothing:antialiased}main{padding-top:3%}@@media screen and (max-width:767px){.row-offcanvas{position:relative}.row-offcanvas-right{right:0}.row-offcanvas-right .sidebar-offcanvas{right:-100%}.sidebar-offcanvas{position:absolute;top:0;width:50%}}
-                    </style>
                     ${criticalStyles} 
+                    ${vendorStyles} 
                     ${styles.map(style => `<link href="/dist/${style.file}" rel="stylesheet"/>`).join('\n')}
-                    ${scripts.map(script => `<script charset="utf-8" src="/dist/${script.file}"></script>`).join('\n')}
                 </head>
                 <body>
                     <div id="react-app">${app}</div>
                     ${criticalScripts}
                     ${mainStyles} 
+                    ${vendorScripts}
+                    ${scripts.map(script => `<script charset="utf-8" src="/dist/${script.file}"></script>`).join('\n')}
                     ${mainScripts}
                 </body>
             </html>`
